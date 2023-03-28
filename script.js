310ee2ec446a05ef62fc29e1f61407b5252b3855
Deadpool1 <atwood169@yahoo.com>
2023-03-27 22:39:18 -0500

    fixed modal addBtn

--- script.js
+++ script.js
@@ -1,34 +1,77 @@
-let myLibrary = [];
+// Array to hold Card Objects
+const myLibrary = [];
 
-const newBook = document.querySelector(".new-book")
-const myModal = document.querySelector("#myModal")
-const MyBtn = document.querySelector("#myBtn")
-const AddBtn = document.querySelector("#add-btn")
+const newBook = document.querySelector(".new-book");
+const myModal = document.querySelector("#myModal");
+const MyBtn = document.querySelector("#myBtn");
+const bookForm = document.getElementById('book-form')
 
-MyBtn.onclick = () => myModal.style.display = "block";
-AddBtn.onclick = () => addBookToLibrary.style.display = "block"
+const addBtn = document.getElementById('add-btn');
+addBtn.addEventListener('click', () => {
+    addBook();
+    renderBooks();
+    closeModal();
+    bookForm.reset();
+})
+
+const closeBtn = document.getElementById('close-btn')
+closeBtn.addEventListener('click', () => {
+    closeModal()
+})
+
+const newBookBtn = document.getElementById('new-book')
+newBookBtn.addEventListener('click', () => {
+    showModal();
+})
 
 
-// constructor
-class Book { 
-    constructor(author, title, pages){
-        this.author = author
-        this.title = title
-        this.pages = pages
+// Constructor
+class Book {
+    constructor(author, title, pages) {
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
     }
 }
 
 // This takes Book constructor
 // Allows modal to create a new card to display on page
+    function addBook() {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const newBook = new Book(title, author, pages);
+    myLibrary.push(newBook);
+};
 
-function addBookToLibrary() {
-    // do stuff here
-    myLibrary.push(Book)
+// Function to render all the card objects on the page
+function renderBooks() {
+    const bookContainer = document.getElementById('book-container');
+    bookContainer.textContent = '';
+    myLibrary.forEach((book) => {
+        const bookElement = document.createElement('div');
+        bookElement.classList.add('book');
+        const titleElement = document.createElement('h2');
+        titleElement.textContent = book.title;
+        const authorElement = document.createElement('p');
+        authorElement.textContent = book.author;
+        const pagesElement = document.createElement('p');
+        pagesElement.textContent = book.pages;
+
+        bookElement.appendChild(titleElement);
+        bookElement.appendChild(authorElement);
+        bookElement.appendChild(pagesElement);
+        console.log(bookElement)
+        bookContainer.appendChild(bookElement);
+    });
 }
 
-const theHobbit = new Book(
-    this.author = "J.R.R Tolkien",
-    this.title = "The Hobbit",
-    this.pages = 304
-)
-console.dir(theHobbit)
+function closeModal() {
+    const modal = document.getElementById('modal');
+    modal.style.display = 'none';
+}
+
+function showModal() {
+    const modal = document.getElementById('modal')
+    modal.style.display = 'block';
+}
